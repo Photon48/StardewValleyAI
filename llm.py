df57b328a30d3721a44614f2bffb95953b638bb3
Rishu Goyal <rishu.goyal@outlook.com.au>
2024-07-12 19:30:47 +1000

    Add files via upload

---
+++ llm.py
@@ -0,0 +1,37 @@
+from openai import OpenAI
+import streamlit as st
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Create Thread:
+thread = client.beta.threads.create()
+
+def run_assistant(prompt):
+    print(prompt)
+    messages = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content= prompt
+    )
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id, assistant_id="asst_rHOJBVNrcUVvFHB6aHxArpUy"
+    )
+
+    messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+
+    message_content = messages[0].content[0].text
+    annotations = message_content.annotations
+    citations = []
+    for index, annotation in enumerate(annotations):
+        message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
+        if file_citation := getattr(annotation, "file_citation", None):
+            cited_file = client.files.retrieve(file_citation.file_id)
+            citations.append(f"[{index}] {cited_file.filename}")
+
+    print(message_content.value)
+    print("\n".join(citations))
+    return f"{message_content.value}\n" + "\n".join(citations)
---
+++ run.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from llm import run_assistant, client
+
+
+st.title("The Stardew Valley Chronicles")
+st.title("__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡̡̡̡.___")
+
+#Initalize Open AI
+# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+st.markdown("Hi Farmer! How can I help you get comfy in Pelican Town?")
+
+
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+        # Display assistant response in chat message container
+
+    with st.chat_message("assistant"):
+        with st.spinner('Thinking...'):
+            response = run_assistant(prompt)
+        response = st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
